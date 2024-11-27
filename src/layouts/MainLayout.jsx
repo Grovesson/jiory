ea063eedff454dab4cdc8f57dab9236e9e98841e
Grove <ad1655@student.jamk.fi>
2024-11-27 13:51:39 +0200

    Added Footer import.

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Nav from "../components/Nav";
+import Footer from "../components/Footer";
 
 const MainLayout = () => {
   return (
