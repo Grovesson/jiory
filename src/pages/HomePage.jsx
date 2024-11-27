8fd0b0ddf0ccc59ddd8760da3ba500659a39b45a
Grove <ad1655@student.jamk.fi>
2024-11-27 14:23:38 +0200

    Fixes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Banner from "../components/Banner";
 import Home from "../components/Home";
-import Footer from "../components/Footer";
 
 const HomePage = () => {
   return (
